4c5386b1ffa882e989e4327c16fe40ebbd7e6af5
Mark Memga <askofmark@gmail.com>
2023-01-14 19:39:35 +0100

    personal details

---
+++ src/PersonalDetails/PersonalDetails.java
@@ -0,0 +1,38 @@
+package PersonalDetails;
+
+public class PersonalDetails {
+    private String firstName;
+    private String lastName;
+    private String location;
+    private String phoneNumber;
+    public PersonalDetails(String firstName, String lastName, String location, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.location = location;
+        this.phoneNumber =phoneNumber;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+}
---
+++ src/PersonalDetails/PersonalDetailsMain.java
@@ -0,0 +1,16 @@
+package PersonalDetails;
+
+public class PersonalDetailsMain {
+    public static void main(String[] args) {
+        PersonalDetails personalDetails = new PersonalDetails("Mark", "Memga", "Lekki", "08164309463");
+        personalDetails.setFirstName("David");
+        System.out.println(personalDetails.getFirstName());
+        personalDetails.setLastName("Memga");
+        System.out.println(personalDetails.getLastName());
+        personalDetails.setLocation("Lekki");
+        System.out.println(personalDetails.getLocation());
+        personalDetails.setPhoneNumber("08164309463");
+        System.out.println(personalDetails.getPhoneNumber());
+        System.out.printf("My name is %s %s,%nI live at %s,%nMy phone number is %s.", personalDetails.getFirstName(), personalDetails.getLastName(), personalDetails.getLocation(), personalDetails.getPhoneNumber());
+    }
+}
