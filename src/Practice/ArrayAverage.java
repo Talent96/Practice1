a14baee720d5e720330a189d0d01684fe8cda2be
Mark Memga <askofmark@gmail.com>
2023-01-24 23:07:21 +0100

    updated

---
+++ src/Practice/ArrayAverage.java
@@ -0,0 +1,30 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class ArrayAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] grade = new double[5];
+        System.out.println("Please Enter " + grade.length + "values");
+        double sum = 0;
+        for(int i = 0; i< grade.length ; i++){
+            grade[i] = input.nextDouble();
+            sum += grade[i];
+        }
+        double average = sum / grade.length;
+        System.out.printf("The average is:%f%n ",average);
+        double max = grade[0];
+        double min = grade[0];
+        for(int i = 0; i< grade.length; i++) {
+            if (grade[i] > max) max = grade[i];
+        }
+        for (int i = 0; i < grade.length; i++) {
+            if (grade[i] < min) min = grade[i];
+
+        }
+        System.out.printf("The maximum value is:%f%n ",max);
+        System.out.printf("The minimum number is:%f%n ",min);
+    }
+
+}
